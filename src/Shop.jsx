bbcdff477d69532ab4a9ecd35829ab9fe34733b9
nitharee <91041973+nthrph@users.noreply.github.com>
2024-10-17 08:46:49 +0000

    imprement add /delete/update

--- src/Shop.jsx
+++ src/Shop.jsx
@@ -1,20 +1,30 @@
 import './Shop.css';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import { useRef } from 'react';
 function Item(props) {
-    return (<div key={props.id} onClick={() => props.callback(props)}>
+    return (<div key={props.id} >
         <img src={props.img} width={200} height={200} /><br />
         id: {props.id} <br />
         name: {props.name}<br />
         price: {props.price}<br />
+        <button onClick={() => props.callback(props)}> add Cart </button>
+        <button onClick={() => props.del_callback(props.id)}> delecte </button>
+        <button onClick={() => props.upd_callback(props)}> update </button>
     </div>);
 }
 
 export default function Shop() {
+    let id;
+    const name_ref = useRef(null);
+    const price_ref = useRef(null);
+    const img_ref = useRef(null);
+
+
     const [products, setProducts] = useState([]);
     const URL = "https://special-system-5p996759v7v3vv-5000.app.github.dev";
     useEffect(() => {
-        axios.get(URL+'/api/products')
+        axios.get(URL + '/api/products')
             .then(response => {
                 setProducts(response.data);
             })
@@ -31,7 +41,7 @@ export default function Shop() {
     //     { id: 3, name: "Notebook MSI Prestige", price: 54900, img: "https://img.advice.co.th/images_nas/pic_product4/A0149954/A0149954_s.jpg" },
     //     { id: 4, name: "Notebook DELL XPS", price: 99900, img: "https://img.advice.co.th/images_nas/pic_product4/A0146335/A0146335_s.jpg" },
     //     { id: 5, name: "Notebook HP Envy", price: 46900, img: "https://img.advice.co.th/images_nas/pic_product4/A0145712/A0145712_s.jpg" }];
-    
+
     const [cart, setCart] = useState([]);
     function addCart(item) {
         setCart([...cart, { id: item.id, name: item.name, price: item.price, img: item.img }]);
@@ -40,7 +50,7 @@ export default function Shop() {
         setCart([]);
     }
 
-    const productList = products.map(item => <Item {...item} callback={addCart} />);
+    const productList = products.map(item => <Item {...item} callback={addCart} del_callback={delProduct} upd_callback={updateProductFrom} />);
     const cartList = cart.map((item, index) => <li>{item.id} {item.name} {item.price}
         <button onClick={() => {
             alert('you click' + index);
@@ -52,7 +62,62 @@ export default function Shop() {
 
     for (let i = 0; i < cart.length; i++) total += cart[i].price;
 
+    function addProduct(){
+        const data = {
+            name : name_ref.current.value,
+            price : price_ref.current.value ,
+           img : img_ref.current.value,
+        }
+        axios.post(URL+"/api/addproduct",data).then((response) => {
+            if(response.data.status=="ok") alert("Add product sucessfully");
+            setProducts(response.data.products);
+        })
+        // alert(name_ref.current.value);
+    }
+
+    function delProduct(id){
+        // event.stopPropogation
+        axios.delete(URL+"/api/delproduct"+id)
+        .then((response) => {
+            if(response.data.status=="ok") alert("Delete product sucessfully");
+            setProducts(response.data.products);
+        })
+        .catch(error=>{
+            console.log("error")
+        });
+    }
+
+    function updateProductFrom(item){
+        id = item.id;
+        name_ref.current.value = item.name;
+        price_ref.current.value = item.price;
+        img_ref.current.value = item.img;
+    }
+
+    function updateProduct(){
+        const data = {
+            name : name_ref.current.value,
+            price : price_ref.current.value,
+            img : img_ref.current.value
+        };
+        if (!id) alert ("no id")
+        axios.put(URL+"/api/updateproduct"+id,data)
+        .then((response) => {
+            if(response.data.status=="ok") alert("Update product sucessfully");
+            setProducts(response.data.products);
+        })
+        .catch(error=>{
+            console.log("error")
+        });
+    }
+
     return (<>
+        name : <input type='text' ref={name_ref} />
+        price : <input type='text'ref={price_ref} />
+        img : <input type='text' ref={img_ref} />
+        <button onClick={addProduct}>add</button>
+        <button onClick={updateProduct}>update</button>
+
         <div className='grid-container'>{productList}</div>
         <h1>Cart</h1>
         <ol>{cartList}</ol>
